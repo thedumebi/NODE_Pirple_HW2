e29c96c35ef97b17ca2a9aebf63cc1976b8624b1
Dumebi <chiwuzohdumebi@gmail.com>
2021-04-21 08:44:23 +0100

    user and token route setup

---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+ *Primary file for the api
+ *
+ */
+
+//  Dependencies
+const server = require("./lib/server");
+
+// Declare the app
+const app = {};
+
+// Init function
+app.init = () => {
+  // Start the server
+  server.init();
+};
+
+// Execute
+app.init();
+
+// Export the app
+module.exports = app;
